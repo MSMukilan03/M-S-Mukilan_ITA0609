db6cdcefb16778f21fa70c123a3574415183e17d
M S MUKILAN <msmukilanmurugan2006@gmail.com>
2026-07-29 10:23:40 +0530

    Delete MsM_13.Parking Free Calculator.py

--- MsM_13.Parking Free Calculator.py
+++
@@ -1,8 +0,0 @@
-print("Parking Fee Calculator")
-vehicle = input("Enter Vehicle Type (Bike/Car): ")
-hours = int(input("Enter Parking Hours: "))
-if vehicle.lower() == "bike":
-    fee = hours * 20
-else:
-    fee = hours * 50
-print("Parking Fee = ₹", fee)
