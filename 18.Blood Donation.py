1a81c6db535e5feade24cb4c663151647eb1c450
M S MUKILAN <msmukilanmurugan2006@gmail.com>
2026-07-29 10:19:05 +0530

    Delete 18.Blood Donation.py

--- 18.Blood Donation.py
+++
@@ -1,8 +0,0 @@
-age = int(input("Enter Age: "))
-weight = float(input("Enter Weight (kg): "))
-hb = float(input("Enter Hemoglobin Level: "))
-
-if age >= 18 and weight >= 50 and hb >= 12.5:
-    print("Eligible to Donate Blood")
-else:
-    print("Not Eligible")
