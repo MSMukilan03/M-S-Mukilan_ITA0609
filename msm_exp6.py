20e546338b58c5680a6b263bed035797af9ab127
M S MUKILAN <msmukilanmurugan2006@gmail.com>
2026-08-02 23:19:34 +0530

    Delete msm_exp6.py

--- msm_exp6.py
+++
@@ -1,7 +0,0 @@
-food = float(input("Food Bill: "))
-gst = food * 0.05
-service = food * 0.10
-total = food + gst + service
-print("GST =", gst)
-print("Service Charge =", service)
-print("Total Bill =", total)
