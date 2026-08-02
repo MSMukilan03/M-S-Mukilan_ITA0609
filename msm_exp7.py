44f3b6567cfe07c2997d46f15f541d8e4b5163cf
M S MUKILAN <msmukilanmurugan2006@gmail.com>
2026-08-02 23:19:58 +0530

    Delete msm_exp7.py

--- msm_exp7.py
+++
@@ -1,7 +0,0 @@
-p = float(input("Principal Amount: "))
-r = float(input("Rate of Interest: "))
-t = float(input("Time (Years): "))
-si = (p * r * t) / 100
-ci = p * ((1 + r / 100) ** t) - p
-print("Simple Interest =", si)
-print("Compound Interest =", ci)
