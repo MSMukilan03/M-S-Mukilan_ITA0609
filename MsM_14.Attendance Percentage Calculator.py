610ab0a346b819b2d202b5a14e6b4040f7f91bb2
M S MUKILAN <msmukilanmurugan2006@gmail.com>
2026-08-02 23:17:36 +0530

    Delete MsM_14.Attendance Percentage Calculator.py

--- MsM_14.Attendance Percentage Calculator.py
+++
@@ -1,9 +0,0 @@
-print("Attendance Calculator")
-total = int(input("Total Classes Conducted: "))
-attended = int(input("Classes Attended: "))
-percentage = (attended / total) * 100
-print("Attendance =", round(percentage, 2), "%")
-if percentage >= 75:
-    print("Eligible for Exam")
-else:
-    print("Not Eligible for Exam")
