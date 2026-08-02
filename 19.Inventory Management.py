32678951fb245d5d4b18bd80284b86c9f226b603
M S MUKILAN <msmukilanmurugan2006@gmail.com>
2026-08-02 23:15:15 +0530

    Delete 19.Inventory Management.py

--- 19.Inventory Management.py
+++
@@ -1,7 +0,0 @@
-stock = int(input("Enter Available Stock: "))
-minimum = int(input("Enter Minimum Stock Level: "))
-
-if stock < minimum:
-    print("Low Stock! Reorder Required.")
-else:
-    print("Stock Level is Sufficient.")
